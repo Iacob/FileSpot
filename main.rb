9f57f56460a05cd6d1e57fce031180ca6b9766c2
Luo Yong <luo.yong.name@gmail.com>
2018-11-10 23:11:20 +0800

    Use timestamp hash as file ID.

--- main.rb
+++ main.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'digest'
 require 'sinatra'
 
 set :port, 8080
 
 set :public_folder, 'web'
+
+FILESPOT_FILE_PATH = "web/files"
 
 # Show current directory
 puts Dir::pwd
@@ -20,7 +23,10 @@ post '/upload' do
   tempfilename = params['file1'][:filename]
   tempfile = params['file1'][:tempfile]
   #
-  FileUtils.cp(tempfile, Dir::pwd + "/web/files/" + tempfilename)
+  file_id = Digest::MD5.hexdigest Time.now.to_f.to_s
+  FileUtils.mkdir(Dir::pwd + "/#{FILESPOT_FILE_PATH}/" + file_id)
+  FileUtils.cp(tempfile, Dir::pwd + "/#{FILESPOT_FILE_PATH}/" + file_id + "/" + tempfilename)
   #puts IO.read(tempfile)
-  return 'uploaded'
+  #return 'uploaded'
+  return "/#{FILESPOT_FILE_PATH}/" + file_id + "/" + tempfilename
 end
